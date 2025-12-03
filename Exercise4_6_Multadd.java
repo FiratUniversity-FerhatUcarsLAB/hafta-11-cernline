31a73309a46f1310cfcb789a3d1b1ec96d280278
Ceren Çam <cerencam001@gmail.com>
2025-12-03 21:18:25 +0300

    Add Exercise 4.6 Multadd and expSum implementation

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,16 +1,23 @@
 public class Exercise4_6_Multadd {
 
     public static double multadd(double a, double b, double c) {
-        // TODO
-        return 0.0;
+        return a * b + c;
     }
 
     public static double expSum(double x) {
-        // TODO
-        return 0.0;
+        return x * Math.exp(-x) + (1 - Math.exp(-x));
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+
+        System.out.println(multadd(1.0, 2.0, 3.0));
+
+        double trig = Math.sin(Math.PI / 4) + Math.cos(Math.PI / 4) / 2;
+        System.out.println(trig);
+
+        double logs = Math.log(10) + Math.log(20);
+        System.out.println(logs);
+
+        System.out.println(expSum(2));
     }
 }
