9df51952e62ce21005a2042171273d8fb8395520
Ceren Çam <cerencam001@gmail.com>
2025-12-03 21:07:45 +0300

    Add Exercise 4.4 with compiler behavior experiments

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,11 +11,21 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
+        // Program calisir, ancak degeri kullanilmadigi icin etkisi olmaz.
+
         getNumber();
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
-
-        // Cevaplarinizi yorum olarak ekleyin.
+        // Derleyici hata verir: void method cannot be used in an expression.
     }
 }
+
+# Exercise 4.4 Answers
+
+1) Bir değer döndüren metodu çağırıp sonucunu kullanmazsak:  
+   Program çalışır ancak dönen değer kullanılmadığı için ek bir çıktı oluşmaz.
+
+2) Void metodu bir ifadenin içinde kullanırsak:  
+   Derleme hatası alınır.  
+   "void cannot be used in an expression"
